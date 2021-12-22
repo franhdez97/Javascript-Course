f3e0ebdb8ab4a45a45f453f3f59402f8de8bd3d4
Francisco Hernández <franhdez97@gmail.com>
2021-12-22 10:37:51 -0600

    se finalizo la prueba

---
+++ activity/1_session/aboutme.js
@@ -0,0 +1,24 @@
+const nombre = "Francisco Hernández";
+const profesion = "Programador";
+const puesto = "Frontend developer";
+
+//Ejercicio 1
+export const ejer1 = `Hola, me llamo ${nombre} y soy ${profesion}. Trabajo en una buena empresa como ${puesto}.`;
+
+//Ejercicio 2
+export const ejer2 = "Se ejecuta primero el contenido del fichero aboutme, por la importación de código."
+
+//Ejercicio 3
+export const ejer3 = "Application, lighthouse y sources."
+
+//Ejercicio 4
+export const ejer4 = () => {
+    for(let i = 1; i <= 100; i++) {
+        if(i % 7 == 0) {
+            console.log(i);
+        }
+    }
+}
+
+//Ejercicio 5
+export const ejer5 = "El atributo MODULE permite ejecutar código javascript de manera estricta, ya que por defecto esta desactivado.";
--- activity/1_session/index.js
+++ activity/1_session/index.js
@@ -1,14 +1,42 @@
+import { ejer1, ejer2, ejer3, ejer4, ejer5 } from './aboutme.js';
+
 // Ejercicio 1
 
 console.log("Ejercicio 1");
+console.log(ejer1);
+console.log("");
 
 // Ejercicio 2
 
+console.log("Ejercicio 2");
+console.log(ejer2);
+console.log("");
+
 // Ejercicio 3
+
+console.log("Ejercicio 3");
+console.log(ejer3);
+console.log("");
 
 // Ejercicio 4
 
+console.log("Ejercicio 4");
+ejer4();
+console.log("");
+
 // Ejercicio 5
+
+console.log("Ejercicio 5");
+console.log(ejer5);
+console.log("");
 
 // Ejercicio 6
 
+console.log("Ejercicio 6");
+const formatter = {
+    prefix: "Hello ",
+    append: function(str) {
+        return `${this.prefix}${str}`;
+    }
+};
+console.log(formatter.append("World"));
