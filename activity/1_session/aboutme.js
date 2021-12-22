e6aa4f9c395cbecba9d44ec5586a562df49884e4
Francisco Hernández <franhdez97@gmail.com>
2021-12-22 17:15:35 -0600

    se hizo cambios para que se vea en el html

--- activity/1_session/aboutme.js
+++ activity/1_session/aboutme.js
@@ -13,8 +13,10 @@ export const ejer3 = "Application, lighthouse y sources."
 
 //Ejercicio 4
 export const ejer4 = () => {
+    const element = document.querySelector("#ejer4");
     for(let i = 1; i <= 100; i++) {
         if(i % 7 == 0) {
+            element.innerHTML += `${i}, `;
             console.log(i);
         }
     }
@@ -22,3 +24,11 @@ export const ejer4 = () => {
 
 //Ejercicio 5
 export const ejer5 = "El atributo MODULE permite ejecutar código javascript de manera estricta, ya que por defecto esta desactivado.";
+
+//Ejercicio 6
+export const ejer6 = {
+    prefix: "Hello ",
+    append: function(str) {
+        return `${this.prefix}${str}`;
+    }
+};
--- activity/1_session/index.js
+++ activity/1_session/index.js
@@ -1,8 +1,9 @@
-import { ejer1, ejer2, ejer3, ejer4, ejer5 } from './aboutme.js';
+import { ejer1, ejer2, ejer3, ejer4, ejer5, ejer6 } from './aboutme.js';
 
 // Ejercicio 1
 
 console.log("Ejercicio 1");
+document.querySelector("#ejer1").innerHTML=ejer1;
 console.log(ejer1);
 console.log("");
 
@@ -10,12 +11,14 @@ console.log("");
 
 console.log("Ejercicio 2");
 console.log(ejer2);
+document.querySelector("#ejer2").innerHTML=ejer2;
 console.log("");
 
 // Ejercicio 3
 
 console.log("Ejercicio 3");
 console.log(ejer3);
+document.querySelector("#ejer3").innerHTML=ejer3;
 console.log("");
 
 // Ejercicio 4
@@ -28,15 +31,15 @@ console.log("");
 
 console.log("Ejercicio 5");
 console.log(ejer5);
+document.querySelector("#ejer5").innerHTML=ejer5;
 console.log("");
 
 // Ejercicio 6
 
 console.log("Ejercicio 6");
-const formatter = {
-    prefix: "Hello ",
-    append: function(str) {
-        return `${this.prefix}${str}`;
-    }
-};
-console.log(formatter.append("World"));
+console.log(ejer6.append("World"));
+console.log(ejer6.append("World"));
+document.querySelector("#ejer6_1").innerHTML=ejer6.append("World");
+//Se añade por prototype
+ejer6.__proto__.toLowerString = str => str.toLowerCase();
+document.querySelector("#ejer6_2").innerHTML=ejer6.toLowerString("I'm Francisco");
